8022badfc51b1445370d51694d09206badea7861
Sagar Jamuar <144097667+sagarjamuar@users.noreply.github.com>
2024-12-08 22:45:16 +0530

    Add files via upload

---
+++ INFORMATICA_FILES/dim_fact_ddl.sql
@@ -0,0 +1,47 @@
+CREATE TABLE dim_state (
+	StateId INT PRIMARY KEY,
+    StateCode VARCHAR(5),
+    StateName VARCHAR(50),
+	create_date DATETIME ,
+	update_date DATETIME
+);
+
+CREATE TABLE dim_county (
+	CountyId INT PRIMARY KEY,
+    CountyFips INT ,
+    CountyName VARCHAR(100),
+    StateId INT,
+	create_date DATETIME ,
+	update_date DATETIME,
+    FOREIGN KEY (StateId) REFERENCES dim_state(StateId),
+);
+
+CREATE TABLE dim_party (
+	PartyId INT PRIMARY KEY,
+    Party VARCHAR(50),
+	create_date DATETIME ,
+	update_date DATETIME,
+);
+
+CREATE TABLE dim_candidate (
+	CandidateId INT PRIMARY KEY,
+    Candidate VARCHAR(100) ,
+    PartyId INT,
+	create_date DATETIME ,
+	update_date DATETIME,
+    FOREIGN KEY (PartyId) REFERENCES dim_party(PartyId),
+);
+
+CREATE TABLE fact_election_results (
+    FactId INT PRIMARY KEY, 
+    CountyId INT,           
+    PartyId INT,            
+    CandidateId INT,        
+    VoteCount FLOAT,        
+    CountyTotalVote INT,    
+	create_date DATETIME ,
+	update_date DATETIME,
+    FOREIGN KEY (CountyId) REFERENCES dim_county(CountyId),
+    FOREIGN KEY (PartyId) REFERENCES dim_party(PartyId),
+    FOREIGN KEY (CandidateId) REFERENCES dim_candidate(CandidateId)
+);
